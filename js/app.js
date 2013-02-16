dcfe405eb81ab25940cdd59d7e94d3a8d182a24f
Fernando Briano <fernando@picandocodigo.net>
2013-02-16 00:50:16 -0200

    Change to 2 spaces tabulation for JS

--- js/app.js
+++ js/app.js
@@ -1,21 +1,21 @@
 function bindEvent(element, type, handler) {
-    if(element.addEventListener) {
-        element.addEventListener(type, handler, false);
-    } else {
-        element.attachEvent('on'+type, handler);
-    }
+  if(element.addEventListener) {
+    element.addEventListener(type, handler, false);
+  } else {
+    element.attachEvent('on'+type, handler);
+  }
 }
 
 bindEvent(document.getElementById("play"), 'click', function(){
-    document.getElementById("audio").play();
+  document.getElementById("audio").play();
 });
 
 bindEvent(document.getElementById("mozilla_app"), 'click', function(){
-    var request = navigator.mozApps.install("http://instantohthehumanity.com/app.webapp");
-    request.onsuccess = function() {
-        alert("App installed");
-    };
-    request.onerror = function() {
-        alert(this.error.name);
-    };
+  var request = navigator.mozApps.install("http://instantohthehumanity.com/app.webapp");
+  request.onsuccess = function() {
+    alert("App installed");
+  };
+  request.onerror = function() {
+    alert(this.error.name);
+  };
 });
