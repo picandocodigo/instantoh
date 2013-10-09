46469bf45bf986e587b0a7085a8e6103c0b7027d
Fernando Briano <fernando@picandocodigo.net>
2013-10-09 16:16:47 -0200

    Fixes manifest url

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ window.onload = function(){
     document.getElementById("install").innerHTML = html
 
     bindEvent(document.getElementById("mozilla_app"), 'click', function(){
-      var request = navigator.mozApps.install("http://instantohthehumanity.com/app.webapp");
+      var request = navigator.mozApps.install("http://instantohthehumanity.com/manifest.webapp");
       request.onsuccess = function() {
         alert("App installed");
       };
